71ea5effb1105824b4e953628e4a141ccb69572e
tammarut <tammarut_nua@true-e-logistics.com>
2021-05-04 00:44:30 +0700

    Make overall layout and header

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,29 @@ import Link from "next/link";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+   return (
+      <div className="mx-auto my-8 w-9/12">
+         <header>
+            <h1 className="text-6xl font-bold text-center">📕My Blog</h1>
+            <nav className="my-4">
+               <ul className="flex flex-row space-x-5 justify-center">
+                  <li>
+                     <Link href="/">
+                        <a>Home</a>
+                     </Link>
+                  </li>
+                  <li>
+                     <Link href="/about">
+                        <a>About</a>
+                     </Link>
+                  </li>
+               </ul>
+            </nav>
+         </header>
 
-      <Component {...pageProps} />
-    </div>
-  );
+         <Component {...pageProps} />
+      </div>
+   );
 }
 
 export default MyApp;
